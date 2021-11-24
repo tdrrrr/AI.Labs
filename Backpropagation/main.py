d3f286a87162c73875458d9dac6d3e1af875768a
Neculau Tudor <neculautudor02@gmail.com>
2021-11-24 12:51:27 +0200

    bckprop

--- Backpropagation/main.py
+++ Backpropagation/main.py
@@ -1,4 +1,13 @@
-import math
+import numpy as np
+lr = 0.1
+epochs = 10000
+inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+expected_output = np.array([[0], [0], [0], [1]])
+inputLayerNeurons, hiddenLayerNeurons, outputLayerNeurons = 2, 2, 1
+hidden_weights = np.random.uniform(size=(inputLayerNeurons, hiddenLayerNeurons))
+hidden_bias = np.random.uniform(size=(1, hiddenLayerNeurons))
+output_weights = np.random.uniform(size=(hiddenLayerNeurons, outputLayerNeurons))
+output_bias = np.random.uniform(size=(1, outputLayerNeurons))
 
 
 def read_from_file(filename):
@@ -6,8 +15,12 @@ def read_from_file(filename):
     return f.read()
 
 
-def sigmoid_function(x):
-    return math.exp(x) / (math.exp(x) + 1)
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
 
 
 def param_read():
@@ -18,5 +31,21 @@ def param_read():
 
 
 if __name__ == '__main__':
-    print(read_from_file("input.txt"))
-    print(sigmoid_function(2))
+    for epoch in range(epochs):
+        # Forward Propagation
+        hidden_layer_activation = np.dot(inputs, hidden_weights)
+        hidden_layer_activation += hidden_bias
+        hidden_layer_output = sigmoid(hidden_layer_activation)
+        output_layer_activation = np.dot(hidden_layer_output, output_weights)
+        output_layer_activation += output_bias
+        predicted_output = sigmoid(output_layer_activation)
+        error = expected_output - predicted_output
+        d_predicted_output = error * sigmoid_derivative(predicted_output)
+        # Backward Propagation
+        error_hidden_layer = d_predicted_output.dot(output_weights.T)
+        d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
+        output_weights += hidden_layer_output.T.dot(d_predicted_output) * lr
+        output_bias += np.sum(d_predicted_output, axis=0, keepdims=True) * lr
+        hidden_weights += inputs.T.dot(d_hidden_layer) * lr
+        hidden_bias += np.sum(d_hidden_layer, axis=0, keepdims=True) * lr
+    print(predicted_output)
