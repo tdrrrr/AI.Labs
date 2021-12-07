cbd8a28a7eebdfe0cee64c63b895a9f369d7cca9
Neculau Tudor <neculautudor02@gmail.com>
2021-12-07 22:51:30 +0200

    second_phase

--- Ontologii/main.py
+++ Ontologii/main.py
@@ -1,8 +1,31 @@
 import nltk
 
+
+def noun_verb_noun(sentence):
+    sentence = nltk.word_tokenize(sentence)
+    tagged_words = nltk.pos_tag(sentence)
+    print(tagged_words)
+    noun1 = False
+    verb = False
+    noun2 = False
+    for i in range(len(tagged_words)):
+        if (tagged_words[i])[1] == 'NN' and not noun2 and not verb:
+            noun1 = True
+        if (tagged_words[i])[1] == 'VB' and noun1 and not noun2:
+            verb = True
+        if (tagged_words[i])[1] == 'NN' and noun2 and verb:
+            noun2 = True
+    return noun1 and verb and noun2
+
+
 if __name__ == '__main__':
-    f = open("test.txt", "r")
+    f = open("text.txt", "r", encoding="utf8")
     text = f.read()
-    split_text = nltk.sent_tokenize(text)
-    print(split_text[0])
+    sentences = nltk.sent_tokenize(text)
+    #for sentence in sentences:
+        #if noun_verb_noun(sentence):
+    print(noun_verb_noun("My money will come when mother calls"))
+
+
+
 
